1d1ec673472a014b159310b7529a04feaac4b8b9
mapsam <matthews.sam@gmail.com>
2016-08-04 09:28:06 -0500

    Nan::CopyBuffer, use scopes

--- src/evilmvt.cpp
+++ src/evilmvt.cpp
@@ -31,38 +31,45 @@ NAN_METHOD(evil)
     protozero::pbf_writer final_tile_writer(final_tile);
 
     // prepare layer pbf_writer to add to final_tile_writer
-    protozero::pbf_writer layer_writer(final_tile_writer, 3);
+    {
+        protozero::pbf_writer layer_writer(final_tile_writer, 3);
 
-    // add layer attributes
-    layer_writer.add_uint32(15, 2); // version
-    std::string name = "layer_name";
-    layer_writer.add_string(1, name.data(), name.size()); // name
+        // add layer attributes
+        std::uint32_t version = 2;
+        layer_writer.add_uint32(15, version); // version
+        std::string name = "layer_name";
+        layer_writer.add_string(1, name.data(), name.size()); // name
 
-    // keys
-    std::string key = "hello";
-    layer_writer.add_string(3, key);
+        // keys
+        std::string key = "hello";
+        layer_writer.add_string(3, key);
 
-    // values
-    protozero::pbf_writer value_writer_string(layer_writer, 4);
-    value_writer_string.add_string(1, "world");
+        // values
+        {
+            protozero::pbf_writer value_writer_string(layer_writer, 4);
+            value_writer_string.add_string(1, "world");
+        }
 
-    // features
-    protozero::pbf_writer feature_writer(layer_writer, 2);
-    std::uint64_t id = 123;
-    feature_writer.add_uint64(1, id); // feature id
-    
-    std::vector<int> tags = { 0, 0 }; // 'hello': 'world'
-    feature_writer.add_packed_int32(1, std::begin(tags), std::end(tags)); // feature tags 
+        // features
+        {
+            protozero::pbf_writer feature_writer(layer_writer, 2);
+            std::uint64_t id = 123;
+            feature_writer.add_uint64(1, id); // feature id
+            
+            std::vector<uint32_t> tags = { 0, 0 }; // 'hello': 'world'
+            feature_writer.add_packed_int32(1, std::begin(tags), std::end(tags)); // feature tags 
 
-    std::uint32_t geom_type = 3; // POLYGON
-    protozero::pbf_writer enum_writer(feature_writer, 3); // GeomType enum
-    enum_writer.add_enum(3, geom_type); // feature geometry type
+            std::uint32_t geom_type = 3; // POLYGON
+            protozero::pbf_writer enum_writer(feature_writer, 3); // GeomType enum
+            enum_writer.add_enum(3, geom_type); // feature geometry type
 
-    std::vector<int> geom = { 9, 50, 34 }; // single point
-    feature_writer.add_packed_int32(4, std::begin(geom), std::end(geom)); // feature geometry
+            std::vector<uint32_t> geom = { 9, 50, 34 }; // single point
+            feature_writer.add_packed_int32(4, std::begin(geom), std::end(geom)); // feature geometry
+        }
+    }
 
     // return the new buffer
-    info.GetReturnValue().Set(Nan::NewBuffer((char*)final_tile.data(), final_tile.size()).ToLocalChecked());
+    info.GetReturnValue().Set(Nan::CopyBuffer((char*)final_tile.data(), final_tile.size()).ToLocalChecked());
     return;
 }
 
--- test/evilmvt.test.js
+++ test/evilmvt.test.js
@@ -1,8 +1,12 @@
 var test = require('tape');
 var evilmvt = require('../lib/index.js');
+var vtinfo = require('@mapbox/vtinfo');
 var fs = require('fs');
 
-test('successfully creates a buffer', function() {
+test('successfully creates a buffer', function(t) {
   var buffer = evilmvt();
   console.log(buffer);
+  var info = vtinfo(buffer);
+  console.log(info);
+  t.end();
 });
