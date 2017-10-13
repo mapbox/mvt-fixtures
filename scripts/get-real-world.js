d2c2e728c01e31d136fd571c1a23fdd5b44ba87c
mapsam <matthews.sam@gmail.com>
2017-10-13 11:49:07 -0700

    use got

--- scripts/get-real-world.js
+++ scripts/get-real-world.js
@@ -1,8 +1,9 @@
 'use strict';
-const request = require('request');
+const got = require('got');
 const SM = require('@mapbox/sphericalmercator');
 const d3 = require('d3-queue');
 const fs = require('fs');
+const path = require('path');
 const extents = require('../lib/real-world-extents.js');
 
 const sm = new SM();
@@ -10,10 +11,9 @@ const q = d3.queue(5);
 
 for (let e in extents) {
   let xyz = sm.xyz(extents[e].bbox, extents[e].zoom);
-  let count = 0;
 
-  if (!fs.existsSync(`real-world/${e}`)) {
-    fs.mkdirSync(`real-world/${e}`);
+  if (!fs.existsSync(path.resolve(path.join(__dirname, '..', 'real-world', e)))) {
+    fs.mkdirSync(path.resolve(path.join(__dirname, '..', 'real-world', e)));
   }
 
   for (let x = xyz.minX; x <= xyz.maxX; x++) {
@@ -30,11 +30,13 @@ q.awaitAll(function(err, data) {
 
 function getAndWriteTile(name, tileset, z, x, y, callback) {
   let url = `https://api.mapbox.com/v4/${tileset}/${z}/${x}/${y}.vector.pbf?access_token=${process.env.MapboxAccessToken}`;
-  request.get(url, function(err, res, data) {
-    if (err) return callback(err);
-    if (res.statusCode !== 200) return callback(new Error(`status code error ${res.statusCode}`));
-    console.log(`writing ${name}/${z}-${x}-${y}.mvt`);
-    fs.writeFileSync(`/Users/mapsam/mapbox/mvt-fixtures/real-world/${name}/${z}-${x}-${y}.mvt`, data);
-    return callback();
-  });
+  got(url)
+    .then(response => {
+      console.log(`writing ${name}/${z}-${x}-${y}.mvt`);
+      fs.writeFileSync(path.join(__dirname, '..', 'real-world', name, `${z}-${x}-${y}.mvt`), response.body);
+      return callback();
+  	})
+    .catch(err => {
+  		return callback(new Error(`error getting ${url}: ${err}`));
+  	});
 };
