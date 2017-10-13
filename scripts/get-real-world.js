54598c1c766bfe7df80f953f4656b53eb7b877c1
mapsam <matthews.sam@gmail.com>
2017-10-13 12:26:23 -0700

    back to request

--- scripts/get-real-world.js
+++ scripts/get-real-world.js
@@ -1,5 +1,5 @@
 'use strict';
-const got = require('got');
+const request = require('request');
 const SM = require('@mapbox/sphericalmercator');
 const d3 = require('d3-queue');
 const fs = require('fs');
@@ -7,13 +7,13 @@ const path = require('path');
 const extents = require('../lib/real-world-extents.js');
 
 const sm = new SM();
-const q = d3.queue(10);
+const q = d3.queue(5);
 
 for (let e in extents) {
   let xyz = sm.xyz(extents[e].bbox, extents[e].zoom);
 
-  if (!fs.existsSync(path.resolve(path.join(__dirname, '..', 'real-world', e)))) {
-    fs.mkdirSync(path.resolve(path.join(__dirname, '..', 'real-world', e)));
+  if (!fs.existsSync(path.join(__dirname, '..', 'real-world', e))) {
+    fs.mkdirSync(path.join(__dirname, '..', 'real-world', e));
   }
 
   for (let x = xyz.minX; x <= xyz.maxX; x++) {
@@ -30,19 +30,11 @@ q.awaitAll(function(err, data) {
 
 function getAndWriteTile(name, tileset, z, x, y, callback) {
   let url = `https://api.mapbox.com/v4/${tileset}/${z}/${x}/${y}.vector.pbf?access_token=${process.env.MapboxAccessToken}`;
-  let dst = path.join(__dirname, '..', 'real-world', name, `${z}-${x}-${y}.mvt`);
-  got.stream(url)
-    .on('error', (err) => {
-      console.log(err);
-      return callback(new Error(`error getting ${url}: ${err}`));
-    })
-    .pipe(fs.createWriteStream(dst))
-    .on('error', (err) => {
-      console.log(err);
-      return callback(new Error(`something errored while writing: ${err}`));
-    })
-    .on('finish', (res) => {
-      console.log(`wrote ${name}/${z}-${x}-${y}.mvt`);
-      return callback();
-    });
+  request.get(url, function(err, res, data) {
+    if (err) return callback(err);
+    if (res.statusCode !== 200) return callback(new Error(`status code error ${res.statusCode}`));
+    console.log(`writing ${name}/${z}-${x}-${y}.mvt`);
+    fs.writeFileSync(path.join(__dirname, '..', 'real-world', name, `${z}-${x}-${y}.mvt`), data);
+    return callback();
+  });
 };
