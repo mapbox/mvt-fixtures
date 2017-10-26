6062c2563e0a4aa1059090c6b7cadd196e0d1b99
mapsam <matthews.sam@gmail.com>
2017-10-26 14:38:11 -0700

    fix all values fixture

--- src/038.js
+++ src/038.js
@@ -14,15 +14,27 @@ module.exports = {
           {
             id: 1,
             tags: [
-              0,
-              0
+              0, 0,
+              1, 1,
+              2, 2,
+              3, 3,
+              4, 4,
+              5, 5,
+              6, 6,
+              7, 7
             ],
             type: 1,
             geometry: [ 9, 50, 34 ]
           }
         ],
         keys: [
-          'allvalues'
+          'string_value',
+          'bool_value',
+          'int_value',
+          'double_value',
+          'float_value',
+          'sint_value',
+          'uint_value'
         ],
         values: [
           { string_value: 'ello' },
@@ -30,7 +42,7 @@ module.exports = {
           { int_value: 6 },
           { double_value: 1.23 },
           { float_value: 3.1 },
-          { sint_value: 87948 },
+          { sint_value: -87948 },
           { uint_value: 87948 }
         ],
         extent: 4096
