95a7b321d7656a789d85ed58fb964d86d99a6be8
mapsam <matthews.sam@gmail.com>
2017-12-07 12:54:39 -0800

    update duplicate layer fixture with checkable tags

--- src/015.js
+++ src/015.js
@@ -1,10 +1,10 @@
 module.exports = {
-  description: 'Includes two layers with the same name value, which is invalid according to the specification. "A layer MUST contain a name field. A Vector Tile MUST NOT contain two or more layers whose name values are byte-for-byte identical."',
+  description: 'Includes two layers with the same name value, which is invalid according to the specification. "A layer MUST contain a name field. A Vector Tile MUST NOT contain two or more layers whose name values are byte-for-byte identical." Decoders can decide to dedupe or fail.',
   specification_reference: 'https://github.com/mapbox/vector-tile-spec/tree/master/2.1#41-layers',
   validity: {
     v1: false,
     v2: false,
-    error: 'fatal'
+    error: 'recoverable'
   },
   json: {
     layers: [
@@ -14,13 +14,17 @@ module.exports = {
         features: [
           {
             id: 1,
-            tags: [],
+            tags: [
+              0, 0
+            ],
             type: 1,
             geometry: [ 9, 50, 34 ]
           }
         ],
-        keys: [],
-        values: [],
+        keys: ['name'],
+        values: [
+          { string_value: 'layer-one' }
+        ],
         extent: 4096
       },
       {
@@ -29,13 +33,17 @@ module.exports = {
         features: [
           {
             id: 1,
-            tags: [],
+            tags: [
+              0, 0
+            ],
             type: 1,
             geometry: [ 9, 62, 84 ]
           }
         ],
-        keys: [],
-        values: [],
+        keys: ['name'],
+        values: [
+          { string_value: 'layer-two' }
+        ],
         extent: 4096
       }
     ]
