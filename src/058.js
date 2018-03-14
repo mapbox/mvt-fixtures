1cb44ccd7aa16f31667b4187d3ad9bf1a12aadae
mapsam <matthews.sam@gmail.com>
2018-03-14 15:40:10 -0700

    huge move_to and line_to commands

--- src/058.js
+++ src/058.js
@@ -1,9 +1,10 @@
 module.exports = {
-  description: 'A point fixture with a gigantic MoveTo command. Can be used to test decoders for memory overallocation situations',
+  description: 'A linestring fixture with a gigantic LineTo command that does not have the equal number of commands after. This can be used to test decoders for memory overallocation situations',
   specification_reference: 'https://github.com/mapbox/vector-tile-spec/tree/master/2.1#43-geometry-encoding',
   validity: {
-    v1: true,
-    v2: true
+    v1: false,
+    v2: false,
+    error: 'fatal'
   },
   json: {
     layers: [
@@ -16,15 +17,15 @@ module.exports = {
             tags: [
               0, 0
             ],
-            type: 1,
-            geometry: [ 4294967289, 2, 2 ]
+            type: 2,
+            geometry: [ 17, 0, 0, 4294967290, 6, 10, 6, 18 ]
           }
         ],
         keys: [
           'command'
         ],
         values: [
-          { string_value: 'move_to'}
+          { string_value: 'line_to'}
         ],
         extent: 4096
       }
