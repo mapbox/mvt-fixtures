aac096eb9b9b4247a6653dea191d2a0ec8e7e4e3
mapsam <matthews.sam@gmail.com>
2018-05-16 11:10:55 -0700

    add new fixtures for compositing tests

---
+++ src/059.js
@@ -0,0 +1,32 @@
+module.exports = {
+  description: 'A layer named "water" with a single feature and unique properties (used as a layer compositing test with fixture 060)',
+  specification_reference: 'https://github.com/mapbox/vector-tile-spec/blob/master/2.1/README.md',
+  validity: {
+    v1: true,
+    v2: true
+  },
+  json: {
+    layers: [
+      {
+        version: 2,
+        name: 'water',
+        features: [
+          {
+            id: 1,
+            tags: [ 0, 0 ],
+            type: 1,
+            geometry: [ 9, 50, 34 ]
+          }
+        ],
+        keys: [
+          'name'
+        ],
+        values: [
+          { string_value: 'mud lake'}
+        ],
+        extent: 4096
+      }
+    ]
+  },
+  proto: '2.1'
+};
